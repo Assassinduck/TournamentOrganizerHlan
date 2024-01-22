75917bb814773114672ac384bca9cb62bc2903f7
Julian pettersen <darlion2301@gmail.com>
2024-01-22 03:29:07 +0100

    Updates

--- src/components/Forms/csgo/CSGOForm.tsx
+++ src/components/Forms/csgo/CSGOForm.tsx
@@ -1,10 +1,24 @@
 import React from "react"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { tournamentSchema } from "../../../../../common/schema"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 export interface CSGOFormProps {
 
 }
 
+
+
 const CSGOFormComponent = (props: CSGOFormProps) => {
+
+    const form = useForm<z.infer<typeof tournamentSchema>>({
+        resolver: zodResolver(tournamentSchema),
+        defaultValues: {
+            
+        },
+    })
+
     return (
         <div>
 
--- src/components/Forms/csgo/schema.ts
+++
@@ -1,5 +0,0 @@
-import * as z from "zod";
-
-const tournamentSchema = z.object({
-    gameName: z.,
-});
